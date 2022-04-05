a08890236ed826992bcf1056cd0b90f2a7694921
TheDodge25 <84635173+TheDodge25@users.noreply.github.com>
2022-04-05 19:02:12 +0300

    Add files via upload

---
+++ pinger.py
@@ -0,0 +1,19 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll
+import requests
+
+phone_number = "9152415333"
+key = ""
+session = requests.Session()
+
+vk_session = vk_api.VkApi(login=phone_number, token=key)
+session_api = vk_session.get_api()
+longpoll = vk_api.longpoll.VkLongPoll(vk_session)
+
+response = session_api.messages.getConversationMembers(peer_id=2000000053, fields="count, profiles")
+
+message = ""
+dict = response['items']
+for item in dict:
+	message += f"@id{str(item['member_id'])} "
+print(message)
