5e88e50c0e3ee4f29ea415609998c2c94d6ba855
leniks <anciborko04@mail.ru>
2022-04-25 00:43:40 +0300

    ыфва

--- access_token.py
+++ access_token.py
@@ -1,1 +1,2 @@
 personal_token = ""
+number = ''
--- trash_deleter.py
+++ trash_deleter.py
@@ -1,42 +1,50 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll
 import requests
+from access_token import personal_token, number
+
 
 A = 10
 B = 20
 limit = 100
 
-phone_number = ""
-key = ""
+phone_number = number
+key = personal_token
 session = requests.Session()
 
 vk_session = vk_api.VkApi(login=phone_number, token=key)
 session_api = vk_session.get_api()
 longpoll = vk_api.longpoll.VkLongPoll(vk_session)
 
-msg_for_delete_count = int(input())
+vk_session.method("messages.send", {"chat_id": 38, "message": "Время скрипту удалять сообщения дождя", "random_id": 0})
 
-
-# vk_session.method("messages.send", {"chat_id": 53, "message": "Время скрипту удалять сообщения 238714865", "random_id": 0})
 
 def deleter(delete_count):
     for i in range(delete_count):
         for event in longpoll.listen():
             if event.type == vk_api.longpoll.VkEventType.MESSAGE_NEW and event.to_me and event.from_chat:
                 user_id = event.user_id
-            chat_id = event.chat_id
-            if chat_id == 53 and user_id == 238714865:  # id чата параллели и id человека
-                message_id = event.message_id
-                #                user_get = vk.users.get(user_ids=user_id)
-                #                user_get = user_get[0]
-                #                first_name = user_get['first_name']
-                #                second_name = user_get['second_name']
-                print(message_id)
+                chat_id = event.chat_id
+                if chat_id == 38 and user_id == 351168531:  # id чата параллели и id человека
+                    message_id = event.message_id
+                    # user_get = vk_api.users.get(user_ids=user_id)
+                    # user_get = user_get[0]
+                    # first_name = user_get['first_name']
+                    # second_name = user_get['second_name']
+                    print(message_id)
                 try:
                     session_api.messages.delete(message_ids=[message_id], delete_for_all=1)
                     vk_session.method("messages.send",
-                                  {"chat_id": 53, "message": f"Удалил сообщение {user_id}", "random_id": 0})
+                                      {"chat_id": 38, "message": f"Удалил сообщение {user_id}", "random_id": 0})
                 except Exception:
-                    print("Часть выброшена", i, user_id)
                     continue
+                print("Часть выброшена", i, user_id)
+                break
+
+    vk_session.method("messages.send",
+                      {"chat_id": 38, "message": f"удаление сообщений {user_id} закончено", "random_id": 0})
     print("Мусор вынесен.")
+
+
+mc = int(input())
+deleter(mc)
